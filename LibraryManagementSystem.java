5175ce28b2e388a1ae03aa56626553a7ce7e810b
jpsherm42 <74579336+jpsherm42@users.noreply.github.com>
2020-12-12 01:57:13 -0500

    template for dropdown menu

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -6,73 +6,56 @@ import java.util.Date;
 
 public class LibraryManagementSystem {
 	
+	
+	/*
+	public static void main(String[] args) {
+		Connection connection = DatabaseConnection.openDatabase();
+		
+		String table = "patrons";
+		String[] bookValues = {"fake title", "fake author", "fake genre"};
+		String[] patronValues = {"fake first", "fake last"};
+		String column = "checkedOut";
+		String value = "false";
+		String keyword = "patron_ID";
+		String criterion = "11";
+		
+		String selectQuery = "SELECT title, author FROM books;";
+		String[] columns = {"Title", "Author"};
+		
+		
+		printFromDatabase(connection, selectQuery, columns);
+		
+		
+		
+		
+	}*/
 	public static void main(String[] args) {
 		
-		
-		
-		
-		
-		
-		// ** USE CASE/default
-		
-		/*
 		// open db connection
 		Connection connection = DatabaseConnection.openDatabase();
 		
-		//boolean usingDB = true;
+		//Patron justinSherman = new Patron(connection, "101");
 		
-		String table = "books";
-		String query = "SELECT * FROM " + table;
-		DatabaseQueries.readFromDatabase(connection, table, query);
+		/*
+		System.out.println(justinSherman.getFine());
+		System.out.println(justinSherman.getFirstName());
+		System.out.println(justinSherman.getLastName());
+		System.out.println(justinSherman.getFullName());
+		System.out.println(justinSherman.getID());
+		System.out.println(justinSherman.getMaxBooks());
+		System.out.println(justinSherman.getMaxHolds());
+		System.out.println(justinSherman.getNumBooksOut());
+		System.out.println(justinSherman.getNumHolds());
 		*/
 		
+		//Patron.createPatron(connection, "fakid", "testman");
+		Patron fakidTestman = new Patron(connection, "11");
+		
+		String keyword = "";
+		String criterion = "Brandon Krakowski";
+		fakidTestman.returnBook(connection, "12");
+		
+		
 	}
-	
-	// search for first and last name
-
-	/*
-	 * Patron functions:
-			Search for books based on input criteria
-				select queries
-			Place a book on hold/remove a hold
-				update
-			Check a book in/out
-				update query
-			Get list of books on hold
-				select query
-			Get list of books checked out
-				select query
-			Get a recommended book
-				select query
-						
-		Librarian functions:
-			Add book to database
-				update query
-			Add patron to database
-				call the Patron constructor; update query
-			Get list of holds by patron, by book, or all holds
-				select query
-			Get list of checked out books
-				select query
-			Determine a book’s overdue status (based on computer’s datetime)
-				select query
-	 */
-	
-	
-	/*
-	 * determine which table keyword to pass
-	 * get the book id from title/author - if this pair exists; error message? // for multiple copies, get min ID ??? 
-	 * determine whether to place value as True or False
-	 * 
-	 * Lib: remove book -need to get book id (if pair exists, etc.)
-	 * lib: add patron - FORM/inputs -> strings -> patron object (call constructor)
-	 * lib: remove patron - get patron id (if pair exists, etc.)
-	 * 
-	 * 
-	 * patron - default search keyword is Title
-	 * 
-	 * FUNCTIOn: getting an iD based on tuple; if it doens't exist, give a nasty message
-	 */
-	
 	
 }
